04caaa571e26b44cb4d66ef5e81643011fb2255c
taurenmage <burneressay@gmail.com>
2024-10-02 01:18:20 -0400

    ep.2 -- game loop & key input

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,8 +1,11 @@
 import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class GamePanel extends JPanel implements Runnable {
+
     // SCREEN SETTINGS
     final int originalTileSize = 16; // 16x16
     final int scale = 3;
@@ -13,12 +16,25 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // currently 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // currently 576 pixels
 
-    Thread gameThread;
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread; // handles the game loop
+
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
+
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); // improves game performance
+
+        this.addKeyListener(keyH); // adds the key listener to the panel
+        this.setFocusable(true); // allows the panel to receive focus
     }
 
     public void startGameThread() {
@@ -30,13 +46,50 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+        double drawInterval = 1000000000 / FPS; // 0.01666 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
         while (gameThread != null) {
 
-            System.out.println("The game loop is running");
+            currentTime = System.nanoTime();
 
-            // 1. UPDATE: update information such as character positions
-            // 2. DRAW: draw the screen
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
 
+            if (delta >= 1) {
+                // 1. UPDATE: update information, such as character positions
+                update();
+                // 2. DRAW: draw the screen, with the updated information
+                repaint();
+                delta--;
+            }
         }
+    }
+
+    public void update() {
+        if (keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed == true) {
+            playerY += playerSpeed;
+        } else if (keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closes the program when the window is closed
+        window.setResizable(false); // window cannot be resized
         window.setTitle("2D Game");
 
         GamePanel gamePanel = new GamePanel();
@@ -14,7 +14,7 @@ public class Main {
 
         window.pack();
 
-        window.setLocationRelativeTo(null);
+        window.setLocationRelativeTo(null); // window location not specified, so it becomes centered
         window.setVisible(true);
 
         gamePanel.startGameThread();
