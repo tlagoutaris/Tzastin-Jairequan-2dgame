43b596c090e42117368ae4a91f52fc96e2b85b6a
taurenmage <burneressay@gmail.com>
2024-10-07 13:13:53 -0400

    fixed camera

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -161,7 +161,7 @@ public class Player extends Entity {
                 break;
         }
 
-        g2.drawImage(image, worldX, worldY, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 
     }
 
